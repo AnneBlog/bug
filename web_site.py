61293843e518539f4560269589faa1f17fed516a
AnneBlog <871993105@qq.com>
2017-03-01 06:14:59 +0800

    Update web_site.py

--- web_site.py
+++ web_site.py
@@ -1,17 +1,15 @@
-* app.py
-    import web  
-    urls = (  
-        '/', 'Index'  
-    )  
-    app = web.application(urls, globals())  
-    render = web.template.render('./templates/')  
+import web  
+urls = (  
+    '/', 'Index'  
+)  
+app = web.application(urls, globals())  
+render = web.template.render('./templates/')  
 
 
-    class Index(object):  
+class Index(object):  
     def GET(self):  
         greeting = "Hello World"  
         return render.index(greeting=greeting)  
-    if __name__ == "__main__":  
-        app.run()
-        
-       
+if __name__ == "__main__":  
+    app.run()
+    
